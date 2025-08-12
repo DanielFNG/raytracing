5638ec8a339066a9dcfb07f54957b4e09becd4c6
Daniel Gordon <danielfng@live.co.uk>
2025-08-12 19:23:05 +0200

    Remove OpenMP test file

--- src/helloworld.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <iostream>
-#include "omp.h"
-
-int main() {
-#pragma omp parallel
-    printf("Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
-}
