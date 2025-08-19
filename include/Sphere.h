dab0825398b56a288576c1cc53a421076fa2ab67
Daniel Gordon <danielfng@live.co.uk>
2025-08-19 15:03:49 +0100

    Explicitly design a copy constructor for Sphere

--- include/Sphere.h
+++ include/Sphere.h
@@ -6,10 +6,15 @@
 #include "Vec3.h"
 #include "Material.h"
 
-class Sphere : public HittableEntity {
+class Sphere : public HittableEntity
+{
 public:
     Sphere(const Vec3& origin, const double radius, const Material& material)
         : HittableEntity(material), origin{origin}, radius{radius}
+    {}
+
+    Sphere(const Sphere& sphere)
+        : HittableEntity(sphere.getMaterial()), origin{sphere.origin}, radius{sphere.radius}
     {}
 
     Hit getRayHit(const Ray& ray, const Interval& interval) const override;
@@ -18,7 +23,5 @@ private:
     Vec3 origin{};
     double radius{};
 };
-
-
 
 #endif //SPHERE_H
