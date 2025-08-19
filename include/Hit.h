552f8f038fdcc2d403506a6a25a40af503d0de0e
Daniel Gordon <danielfng@live.co.uk>
2025-08-19 15:01:42 +0100

    Have hit be a struct instead of a class

--- include/Hit.h
+++ include/Hit.h
@@ -4,28 +4,20 @@
 #include "Vec3.h"
 #include "Material.h"
 
-class Hit {
-public:
+struct Hit
+{
+    double t{};
+    Vec3 point{};
+    Vec3 normal{};
+    const Material* material{nullptr};
+
     Hit() = default;
 
     Hit(const double t, const Vec3& point, const Vec3& normal, const Material& material)
         : t{t}, point{point}, normal{normal}, material{&material}
     {}
 
-    const Vec3& getNormal() const {return normal;}
-    double getT() const {return t;}
-    const Vec3& getPoint() const {return point;}
-    const Material* getMaterial() const {return material;}
-
     explicit operator bool() const {return material != nullptr;}
-
-private:
-    double t{};
-    Vec3 point{};
-    Vec3 normal{};
-    const Material* material{nullptr};
-    // Choosing const Material* over & so that we can have failed hits with nullptr materials
 };
-
 
 #endif //HIT_H
