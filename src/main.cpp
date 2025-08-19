7531a45d1072e28f1def91ac9cf64b2cab58b5bc
Daniel Gordon <danielfng@live.co.uk>
2025-08-19 15:12:00 +0100

    Add hollow sphere example

--- src/main.cpp
+++ src/main.cpp
@@ -8,27 +8,20 @@ int main()
     constexpr Vec3 camera_origin{0,0,0};
     constexpr CameraConfig camera_config {};
     Camera camera{camera_config, camera_origin};
-    //Material reflective{.reflective = true, .albedo = Vec3{0.8, 0.8, 0.8}};
-    //Material nonreflective{.reflective = false, .albedo = Vec3{0.8, 0.6, 0.2}};
-    Material lambertian1{.reflective = false, .albedo = Vec3{0.8, 0.8, 0.0}, .refractive_index = 0.0};
-    //Material lambertian2{.reflective = false, .albedo = Vec3{0.1, 0.2, 0.5}};
-    Material metal1{.reflective = true, .fuzz = 0.3, .albedo = Vec3{0.8, 0.8, 0.8}, .refractive_index = 0.0};
-    Material metal3{.reflective = true, .fuzz = 0.0, .albedo = Vec3{0.1, 0.2, 0.5}, .refractive_index = 0.0};
-    Material glass{.reflective = false, .fuzz = 0.0, .albedo = Vec3{1.0, 1.0, 1.0}, .refractive_index = 1.5};
+
+    const Lambertian ground{Vec3{0.8, 0.8, 0.0}};
+    const Lambertian lambertian{Vec3{0.1, 0.2, 0.5}};
+    const Reflector metal{Vec3{0.8, 0.6, 0.2}, 1.0};
+    const Refractor glass{Vec3{1.0, 1.0, 1.0}, 1.5};
+    const Refractor air{Vec3{1.0, 1.0, 1.0}, 1.0};
 
     Scene world{1.0};
-    world.add(Sphere{Vec3{0,-100.5,-1}, 100.0, lambertian1});
-    world.add(Sphere{Vec3{0.0,0.0,-1.2}, 0.5, metal3});
-    world.add(Sphere{Vec3{-1.0,0.0,-1.0}, 0.5, metal1});
-    world.add(Sphere{Vec3{1.0,0.0,-1.0}, 0.5, glass});
-    /*
-    world.add(Sphere{Vec3{0,0,-1}, 0.5, reflective});
-    world.add(Sphere{Vec3{-4,1,-3}, 0.3, nonreflective});
-    world.add(Sphere{Vec3{4.0,0.5,-7}, 2.0, reflective});
-    world.add(Sphere{Vec3{-30,20,-50}, 30, reflective});
-    world.add(Sphere{Vec3{2.0,0,-1}, 0.5, nonreflective});
-    world.add(Sphere{Vec3{0, -100.5, -1}, 100, nonreflective});
-    */
+    const Sphere sphere{Sphere{Vec3{0,-100.5,-1}, 100.0, ground}};
+    world.add(sphere);
+    world.add(Sphere{Vec3{0.0,0.0,-1.2}, 0.5, lambertian});
+    world.add(Sphere{Vec3{-1.0,0.0,-1.0}, 0.5, glass});
+    world.add(Sphere{Vec3{-1.0,0.0,-1.0}, 0.4, air});
+    world.add(Sphere{Vec3{1.0,0.0,-1.0}, 0.5, metal});
 
     camera.render(world);
 }
