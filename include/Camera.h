c4069b593e1036356a9c9f35eacdc948dc74c67a
Daniel Gordon <danielfng@live.co.uk>
2025-08-20 18:41:24 +0100

    Add configurable camera with depth of field

--- include/Camera.h
+++ include/Camera.h
@@ -10,17 +10,22 @@ struct CameraConfig
 {
     double aspect_ratio{16.0/9.0};
     int image_width{800};
-    double viewport_height{2.0};
-    double focal_length{1.0};
+    double focus_distance{1.0};
+    double field_of_view{90};
+    double defocus_angle{0};
     int anti_aliasing_samples{50};
     int max_depth{10};
 };
 
 class Camera {
 public:
-    Camera(const CameraConfig& input_config, const Vec3& origin);
+    Camera(const CameraConfig& input_config, const Vec3& origin, const Vec3& direction = {0.0, 0.0, -1.0}, double twist = 0.0);
 
+    void lookAt(const Vec3& position);
+    void rotate(double angle);
     void updateConfig(const CameraConfig& new_config);
+
+    Vec3 sampleOrigin() const;
 
     void render(const Scene& scene, bool parallel = true) const;
 
@@ -41,12 +46,22 @@ private:
     Vec3 background_colour(const Ray& ray) const;
 
     CameraConfig config{};
+
     Vec3 origin{};
+    Vec3 direction{};
+    double twist{};
 
     int image_height{};
+    double viewport_height{};
     double viewport_width{};
+
+    Vec3 viewport_dx{};
+    Vec3 viewport_dy{};
+    Vec3 viewport_dz{};
     Vec3 pixel_dx{};
     Vec3 pixel_dy{};
+    Vec3 defocus_region_dx{};
+    Vec3 defocus_region_dy{};
 
     Vec3 viewport_origin{};
     Vec3 pixel_origin{};
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -8,10 +8,22 @@
 #include "Interval.h"
 #include <execution>
 
-Camera::Camera(const CameraConfig& input_config, const Vec3& origin)
-    : config{input_config}, origin{origin}
+Camera::Camera(const CameraConfig& input_config, const Vec3& origin, const Vec3& direction, const double twist)
+    : config{input_config}, origin{origin}, direction{direction}, twist{twist}
 {
     deriveCameraParameters();
+    deriveGeometricParameters();
+}
+
+void Camera::lookAt(const Vec3& position)
+{
+    direction = position - origin;
+    deriveGeometricParameters();
+}
+
+void Camera::rotate(const double angle)
+{
+    twist = angle;
     deriveGeometricParameters();
 }
 
@@ -30,15 +42,26 @@ void Camera::updateParameters()
 void Camera::deriveCameraParameters()
 {
     image_height = std::max(static_cast<int>(config.image_width/config.aspect_ratio), 1);
-    viewport_width = config.viewport_height * static_cast<double>(config.image_width)/image_height;
-    pixel_dx = Vec3{viewport_width/config.image_width, 0, 0};
-    pixel_dy = Vec3{0, -config.viewport_height/image_height, 0};
+    viewport_width = 2*std::tan(config.field_of_view*M_PI/180.0/2)*config.focus_distance;
+    viewport_height = viewport_width * static_cast<double>(image_height)/config.image_width;
 }
 
 void Camera::deriveGeometricParameters()
 {
-    viewport_origin = Vec3{origin + Vec3{-viewport_width/2, config.viewport_height/2, -config.focal_length}};
+    const double twist_rad{twist*M_PI/180.0};
+    const Vec3 twist_vector{-std::sin(twist_rad),std::cos(twist_rad),0};
+    viewport_dz = normalised(-direction);
+    viewport_dx = normalised(cross(twist_vector, viewport_dz));
+    viewport_dy = cross(viewport_dz, viewport_dx);
+    const Vec3 scaled_viewport_dx{viewport_dx*viewport_width};
+    const Vec3 scaled_viewport_dy{-viewport_dy*viewport_height};
+    pixel_dx = scaled_viewport_dx/config.image_width;
+    pixel_dy = scaled_viewport_dy/image_height;
+    viewport_origin = Vec3{origin - config.focus_distance*viewport_dz - scaled_viewport_dx/2 - scaled_viewport_dy/2};
     pixel_origin = Vec3{viewport_origin + 0.5 * (pixel_dx + pixel_dy)};
+    const double defocus_radius{config.focus_distance*std::tan(config.defocus_angle/2*M_PI/180.0)};
+    defocus_region_dx = viewport_dx*defocus_radius;
+    defocus_region_dy = viewport_dy*defocus_radius;
 }
 
 void Camera::render(const Scene& scene, const bool parallel) const
@@ -123,9 +146,27 @@ std::vector<Vec3> Camera::samplePixel(const Vec3& pixel_location) const
     return subpixel_locations;
 }
 
+Vec3 Camera::sampleOrigin() const
+{
+    while (true)
+    {
+        const Vec3 point_in_unit_disc
+        {
+            Random::getRandom(DefinedIntervals::unit),
+            Random::getRandom(DefinedIntervals::unit),
+            0.0
+        };
+        if (point_in_unit_disc.length_squared() < 1)
+        {
+            return origin + point_in_unit_disc[0] * defocus_region_dx + point_in_unit_disc[1] * defocus_region_dy;
+        }
+    }
+}
+
 Vec3 Camera::colourSubpixel(const Vec3& subpixel_location, const Scene& scene) const
 {
-    Ray ray_to_pixel{origin, subpixel_location - origin, scene.getRefractiveIndex()};
+    const Vec3 ray_origin{sampleOrigin()};
+    Ray ray_to_pixel{ray_origin, subpixel_location - ray_origin, scene.getRefractiveIndex()};
     return ray_colour(ray_to_pixel, scene);
 }
 
@@ -163,7 +204,7 @@ Vec3 Camera::ray_colour(Ray& ray, const Scene& scene) const
 
 Vec3 Camera::background_colour(const Ray& ray) const
 {
-    Vec3 direction{normalised(ray.getDirection())};
-    const double a {0.5*(direction[1] + 1.0)};
+    Vec3 normalised_direction{normalised(ray.getDirection())};
+    const double a {0.5*(normalised_direction[1] + 1.0)};
     return Vec3{(1 - a)*Vec3{1,1,1} + a*Vec3{0.5,0.7,1.0} };
 }
